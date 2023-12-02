a37133915a749f0566aa38730f94885f3a5df8fc
Hakim Ahmed <hakimahmed@MacBook-Pro.local>
2023-12-02 21:36:30 +0100

    Day1 resolved

--- day1/main.py
+++ day1/main.py
@@ -1,5 +1,20 @@
+import re
+
+
+def read_file(file_name="input") -> str:
+    with open(file_name, 'r') as f:
+        return f.read()
+
+
 def main():
-    pass
+    data = read_file()
+    lines = data.split('\n')
+    sum = 0
+    for line in lines:
+        digits = re.findall(r'[0-9]', line)
+        sum += int(str(digits[0]) + str(digits[-1]))
+
+    print('Result: ', sum)
 
 
 if __name__ == "__main__":
